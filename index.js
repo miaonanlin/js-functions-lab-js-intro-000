6d6a4117f048da409a85cb6837e6219e7970477a
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-30 03:58:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 function happyHolidays(){
   return("Happy holidays!")
 }
-
-function happyHolidaysTo(name){
-  return("Happy holidays," ${name}"!")
-}
-
-function happyCustomHolidayTo(holiday,name){
-  return("Happy ${holiday}, ${name}!")
-}
-happyCustomHolidayTo("Independence Day", "you")
-
-
-function holidayCountdown(holiday, days) {
-  return("It's ${days} days until ${holiday}!")
-}
-holidayCountdown("Mother", 20)
